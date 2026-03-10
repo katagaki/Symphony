a1665aebf98d45e97b9c009a047f5e774ac5f7c8
katagaki <68231035+katagaki@users.noreply.github.com>
2026-03-11 07:18:20 +0900

    Initial Commit

---
+++ Symphony/SymphonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymphonyApp.swift
+//  Symphony
+//
+//  Created by シン・ジャスティン on 2026/03/11.
+//
+
+import SwiftUI
+
+@main
+struct SymphonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
