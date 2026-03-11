8d87c26e7ba040069a8f67f36ded2a1a262800e2
Justin <68231035+katagaki@users.noreply.github.com>
2026-03-11 16:36:49 +0900

    AppsListView.swift を更新

--- Symphony/Views/Apps/AppsListView.swift
+++ Symphony/Views/Apps/AppsListView.swift
@@ -69,7 +69,7 @@ struct AppsListView: View {
         }
         .navigationTitle("Apps.Title")
         .toolbar {
-            ToolbarItemGroup(placement: .topTrailingBar) {
+            ToolbarItemGroup(placement: .topBarTrailing) {
                 Menu {
                     Picker("Apps.SortBy", selection: $sortOrder) {
                         ForEach(AppSortOrder.allCases, id: \.self) { order in
@@ -81,7 +81,7 @@ struct AppsListView: View {
                 }
             }
             ToolbarSpacer(.fixed)
-            ToolbarItemGroup(placement: .topTrailingBar) {
+            ToolbarItemGroup(placement: .topBarTrailing) {
                 Menu {
                     Button(role: .destructive) {
                         authManager.signOut()
